60c72973245df4b2664030af7bf51a1515ae8f83
G V V S S Datta MANIKANTA <23mh1a4918@acoe.edu.in>
2025-12-30 20:07:51 +0530

    Add documentation to frontend entry point

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,14 @@
+/**
+ * Frontend Application Entry Point
+ * Initializes React app with routing and strict mode
+ */
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+// Mount React application to DOM
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
